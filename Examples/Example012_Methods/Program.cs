dbe0837b217546ffbb46d3389271406c771c3b0c
Julia <granovskaya.97@mail.ru>
2022-07-03 16:18:40 +0300

    Add multiplication table

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -60,3 +60,12 @@
 // }
 // string res = Method4(10, "asdf");
 // Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
