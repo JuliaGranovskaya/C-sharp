9f6af4394539a27b5dff77b5d6f6f475875eaf37
Julia <granovskaya.97@mail.ru>
2022-07-03 16:09:48 +0300

    Add for

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -47,3 +47,16 @@
 // }
 // string res = Method4(10, "asdf");
 // Console.WriteLine(res);
+
+// Цикл for
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for( int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
